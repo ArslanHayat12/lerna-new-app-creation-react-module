ab42a84dc07996290f697084b1373fcf447bc704
Arslan Hayat <muhammadarslanhayat@gmail.com>
2019-06-10 10:34:50 +0500

    Import cs

--- integeration/my-app/src/App.tsx
+++ integeration/my-app/src/App.tsx
@@ -1,28 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { HelloWorld }from "@lerna-new-app-creation-react-module/module1";
+import React from "react";
+import "./App.css";
+import { InfiniteScroll } from "@lerna-new-app-creation-react-module/module1";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <HelloWorld />
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InfiniteScroll />
     </div>
   );
-}
+};
 
 export default App;
